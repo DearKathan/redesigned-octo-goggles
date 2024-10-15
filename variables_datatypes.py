6cb084ac26ae46c1e9f2ba967b508dcec2c6540c
DearKathan <kathanshah2006@gmail.com>
2024-10-15 21:48:36 +0530

    uploading variables_datatypes file

---
+++ variables_datatypes.py
@@ -0,0 +1,104 @@
+# x = 6
+
+# print( x )
+# print( 5 * x + 8 - x)  
+
+# s, t, v = 4, 6, 7
+# print(s)
+# print(t)
+# print(v)
+# print(s, t, v)
+
+# print("hey", end= ",")
+# print("me", end= "")
+# print("what's up?")
+
+# print("hello", "world", sep="..", end='')
+# print("rajsa")
+
+# print("hello", "world" "there", sep= "..", end= " ")
+# print("!")
+
+# print("Enter input")
+# user_input= input()
+# print("User has entered input:", user_input)
+
+# accept= input("Plz enter input: ")
+# print("user had entered input:", accept)
+
+
+# a= 93
+# b= 29.29
+# c= "slr"
+# d= 'l'
+# e= True
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(d))
+# print(type(e))
+# Helps us to print the datatype of variable
+# EVERYTHING IS AN OBJECT IN PYTHON!!
+
+
+# x= "75"
+# y= '75'
+# z= 75
+# print(type(x))
+# print(type(y))
+# print(type(z))
+
+# converted_value_a= int(x)
+# converted_value_b= float(y)
+
+# print(converted_value_a)
+# print(type(converted_value_a))
+
+# print(converted_value_b)
+# print(type(converted_value_b))
+
+# t="494jf"
+# con=float(t)
+# print(type(con))
+
+# str1= "yo"
+# str2= "there"
+# str3= str1+str2
+# print(str3)
+
+
+# x= 3
+# y= 20
+# print("Addition=", x+y)
+# print("Multiplication=", x*y)
+# print("x+y")
+# print("Subtraction:", x-y)
+# print("Division-", x/y) Quotient printing
+# print("Exponent-", y ** x) 
+# print("Modulo-",y% x) Remainder printing
+# print("Integer division-",17 // 2) Floor, Ceiling logic
+
+
+# print(7>=8)
+# a= 8
+# b= 3
+# print(a!=b) 
+# print(a<b)
+# print(a>=b)
+# c= 7
+# d= 7
+# print(d>=c)
+
+# x=7
+# x= x +3 Or
+# x +=3
+# print(x)
+# Both are the same
+# x= x - 3
+# x -= 3
+
+# arr= [2,4,5,7.9,False, "RST", [5,8]]
+# print(arr)
+# print(type(arr))
+# print(arr[6][1])
+
