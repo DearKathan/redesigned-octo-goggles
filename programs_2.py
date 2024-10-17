4ddea0896465dac463ba1d99397c00b93fcf17e2
DearKathan <kathanshah2006@gmail.com>
2024-10-17 21:38:09 +0530

    Add files via upload

---
+++ programs_2.py
@@ -0,0 +1,15 @@
+# Basic Function Creation: Write a function named greet that takes a name as an argument and prints “Hello,[name!”. Function with Default Argument: Create a function describe_pet that takes the name of a pet and its type (default to “dog”) and prints a description.'
+
+# Return Multiple Values: Write a function calculate that takes two numbers and returns their sum, difference, product, and quotient.
+
+# Variable Length Arguments: Create a function sum_all that accepts any number of arguments and returns their sum.
+
+# Function with Keyword Arguments: Create a function build_profile that accepts a first name, last name, and arbitrary keyword arguments, and prints a user profile.
+
+# Recursive Function: Write a recursive function factorial that calculates the factorial of a given number.
+
+# Higher-Order Function: Write a function apply_function that takes another function and a value, and returns the result of applying the function to the value.
+
+# Inner Function: Create a function outer that contains an inner function inner. The outer function should call the inner function and return its result.
+
+# Lambda Function: Write a lambda function that takes two arguments and returns their product. Assign it to a variable named multiply.
